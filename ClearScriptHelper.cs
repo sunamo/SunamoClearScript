b8ed81d253fe566789444bbdd9bfb7136215fc7b
sunamo <radek.jancik@sunamo.cz>
2024-01-05 12:57:26 +0100

    .

--- ClearScriptHelper.cs
+++ ClearScriptHelper.cs
@@ -1,33 +1,32 @@
-namespace SunamoClearScript
+namespace SunamoClearScript;
+
+public class ClearScriptHelper
 {
-    public class ClearScriptHelper
+    V8ScriptEngine engine = new V8ScriptEngine();
+
+    public static ClearScriptHelper ci = new ClearScriptHelper();
+
+    private ClearScriptHelper()
     {
-        V8ScriptEngine engine = new V8ScriptEngine();
 
-        public static ClearScriptHelper ci = new ClearScriptHelper();
+    }
 
-        private ClearScriptHelper()
+    public bool Execute(string c)
+    {
+        try
         {
-
+            engine.Compile(c);
+            //engine.Execute(c);
         }
-
-        public bool Execute(string c)
+        catch (Exception ex)
         {
-            try
-            {
-                engine.Compile(c);
-                //engine.Execute(c);
-            }
-            catch (Exception ex)
-            {
-                return false;
-            }
-            return true;
+            return false;
         }
+        return true;
+    }
 
-        ~ClearScriptHelper()
-        {
-            engine.Dispose();
-        }
+    ~ClearScriptHelper()
+    {
+        engine.Dispose();
     }
 }
